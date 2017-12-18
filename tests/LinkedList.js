afbb4f6c7599775a5926fb06c57adb3275a785ca
Valentino Valentin <valentinovalentine95@gmail.com>
2017-12-18 08:06:38 -0700

    running tests

--- tests/LinkedList.js
+++ tests/LinkedList.js
@@ -1,148 +1,400 @@
 import { expect } from 'chai';
+import Node from '../scripts/Node'
 import LinkedList from '../scripts/LinkedList'
 
-describe('list should have a head', () => {
-  var list
+describe('LINKED LIST', () => {
+  let list;
 
   beforeEach(() => {
     list = new LinkedList();
   });
 
-  it ('list should have null head', () => {
-    expect(list.head).to.equal(null);
+  it('should start with zero elements', () => {
+    expect(list.length).to.eq(0);
+  });
+
+  it('should set its default head to null', () => {
+    expect(list.head).to.eq(null);
+  });
+
+  describe('UNSHIFT', () => {
+    it('should add items to front of list / head', () => {
+      list.unshift('duck');
+      expect(list.length).to.equal(1)
+      expect(list.head.data).to.equal('duck')
+
+      list.unshift('goose');
+      expect(list.length).to.equal(2)
+      expect(list.head.data).to.equal('goose')
+      expect(list.head.next.data).to.equal('duck')
+    })
   })
 
-  it ('should allow me to prepend' , () => {
-    list.prepend(29);
-    list.prepend(30);
+  describe('SHIFT', () => {
+    it.skip('should return null if nothing in the list', () => {
+      let result = list.shift();
 
-    expect(list.head.data).to.equal(30);
+      expect(result).to.equal(null)
+    })
+
+    it.skip('should return the first item in the list', () => {
+      list.unshift('duck');
+
+      expect(list.head.data).to.equal('duck');
+
+      let result = list.shift();
+
+      expect(result.data).to.equal('duck');
+    })
+
+    it.skip('should remove items from the front of list / head', () => {
+
+      list.unshift('duck');
+      list.unshift('goose');
+      expect(list.head.data).to.equal('goose');
+      expect(list.head.next.data).to.equal('duck');
+
+      // remove goose from list
+      result = list.shift();
+      expect(result.data).to.equal('goose')
+
+      // duck should now be back at the start of the list
+      expect(list.head.data).to.equal('duck')
+
+      // remove duck from list
+      result = list.shift();
+
+      expect(result.data).to.equal('duck')
+
+      // no more items in our list
+      expect(list.head).to.equal(null)
+    })
   })
 
-  it ('should allow me to shift' , () => {
-    list.prepend(29);
-    list.prepend(30);
+  describe('PUSH', () => {
+    it.skip('should push a single element to a list', () => {
+      list.push('duck');
+      expect(list.head.data).to.eq('duck');
+    });
 
-    expect(list.head.data).to.equal(30);
+    it.skip('should increment the length of the list', () => {
+      list.push('duck');
+      expect(list.length).to.eq(1);
+    });
 
-    let node = list.shift();
-    expect(list.head.data).to.equal(29);
-    expect(node.data).to.equal(30);
-  })
+    it.skip('should increment the length count', () => {
+      list.push('duck');
+      list.push('goose');
+      list.push('swan');
+      expect(list.length).to.eq(3);
+    });
 
-  it ('should allow me find the end/tail' , () => {
-    list.prepend(29);
-    list.prepend(30);
-    list.prepend(25);
-    list.prepend(18);
-    list.prepend(303);
+    it.skip('should assign the head to the first element pushed', () => {
+      expect(list.head).to.eq(null);
 
-    let node = list.findTail();
+      list.push('duck');
+      expect(list.head.data).to.eq('duck');
 
-    expect(node.data).to.equal(29);
-  })
+      list.push('goose');
+      expect(list.head.data).to.eq('duck');
+    });
 
-  it ('should append', () => {
-    list.prepend(30);
-    list.prepend(25);
-    list.prepend(18);
-    list.append(17);
+    it.skip('should attach the second element to the first element', () => {
+      list.push('duck');
+      list.push('swan');
+      expect(list.head.data).to.eq('duck');
+      expect(list.head.next.data).to.eq('swan');
+    });
 
-    let node = list.findTail();
-    expect(node.data).to.equal(17);
+    it.skip('should attach nexts in sequential order', () => {
+      list.push('duck');
+      list.push('goose');
+      list.push('swan');
+      list.push('southern screamer');
+      expect(list.head.data).to.eq('duck');
+      expect(list.head.next.data).to.eq('goose');
+      expect(list.head.next.next.data).to.eq('swan');
+      expect(list.head.next.next.next.data).to.eq('southern screamer');
+    });
   });
 
-  it ('pop should remove one from the end', () => {
-    list.prepend(30);
-    list.prepend(25);
-    list.prepend(18);
+  describe('POP', () => {
+    it.skip('should return null if nothing in list', () => {
+      expect(list.length).to.eq(0);
+      expect(list.pop()).to.eq(null);
+    });
 
-    let node = list.findTail();
+    it.skip('should not decrement the length if there are no nodes', () => {
+      list.pop()
+      expect(list.length).to.eq(0);
+    });
 
-    expect(node.data).to.equal(30);
+    it.skip('should change the length', () => {
+      list.push('duck');
 
-    let poppedNode = list.pop();
-    expect(poppedNode.data).to.equal(30)
+      expect(list.length).to.eq(1)
 
-    let newLastNode = list.findTail();
-    expect(newLastNode.data).to.equal(25);
+      let result = list.pop();
+
+      expect(list.length).to.eq(0);
+    });
+
+    it.skip('should set the list head to null', () => {
+      list.push('duck');
+      let result = list.pop();
+
+      expect(list.head).to.eq(null);
+    });
+
+    it.skip('should return the last element', () => {
+      list.push('duck');
+      const result = list.pop();
+
+      expect(result.data).to.eq('duck');
+    });
+
+    it.skip('should return the last element from the list', () => {
+      list.push("duck");
+      list.push("goose");
+      list.push("swan");
+      list.push("southern screamer");
+
+      const output = list.pop();
+      expect(output.data).to.eq('southern screamer');
+    });
+
+    it.skip('should remove the last element from the list', () => {
+        list.push("duck");
+        list.push("goose");
+        list.push("swan");
+
+        const output = list.pop();
+        expect(output.data).to.eq('swan');
+        expect(list.length).to.eq(2);
+
+        const output2 = list.pop();
+        expect(output2.data).to.eq('goose');
+        expect(output2.next).to.eq(null);
+        expect(list.length).to.eq(1);
+
+        const output3 = list.pop();
+        expect(output3.data).to.eq('duck');
+        expect(output3.next).to.eq(null);
+        expect(list.length).to.eq(0);
+      });
   });
 
-  it ('find should find node in list', () => {
-    list.prepend(30);
-    list.prepend(25);
-    list.prepend(18);
+  describe('FIND', () => {
+    beforeEach(() => {
+      list.push('oh');
+      list.push('hello');
+      list.push('world');
+    });
 
-    let node = list.find(25);
+    it.skip('should return the node if it is found', () => {
+      const result = list.find('hello');
 
-    expect(node.data).to.equal(25);
+      expect(result.data).to.eq('hello');
+      expect(result.next.data).to.eq('world');
+    });
+
+    it.skip('should return true the node if node in list', () => {
+      const result = list.find('world');
+
+      expect(result.data).to.eq('world');
+      expect(result.next).to.eq(null);
+    });
+
+    it.skip('should return null if node is missing', () => {
+      const result = list.find("nope");
+
+      expect(result).to.eq(null);
+    });
   });
 
-  it ('if no node found return "not found"', () => {
-    list.prepend(30);
-    list.prepend(25);
-    list.prepend(18);
+  describe('LAST NODE', () => {
+    context('with several nodes', () => {
+      beforeEach(() => {
+        list.push('The');
+        list.push('rain');
+        list.push('in');
+        list.push('Spain');
+      });
 
-    let node = list.find(252);
+      it.skip('finds the last node', () => {
+        expect(list.getLast().data).to.eq('Spain');
+      });
+    });
 
-    expect(node).to.equal('not found');
+    context('with one node', () => {
+      beforeEach(() => {
+        list.push('Ahoy!');
+      });
+
+      it.skip('finds the only node', () => {
+        expect(list.getLast().data).to.eq('Ahoy!');
+      });
+    });
+
+    context('with no nodes', () => {
+      it.skip('returns null', () => {
+        expect(list.getLast()).to.eq(null);
+      });
+    });
   });
 
-  it ('length should start at 0', () => {
-    expect(list.length).to.equal(0);
+  describe('DELETE', () => {
+    it.skip('deletes a solo node', () => {
+      list.push('hello');
+      list.delete('hello');
+      expect(list.length).to.eq(0);
+      expect(list.head).to.eq(null);
+    });
+
+    it.skip('does not perform a delete when a node does not match', () => {
+      list.push('hello');
+      list.delete('goodbye');
+      expect(list.length).to.eq(1);
+      expect(list.head.data).to.eq('hello');
+    });
+
+    context('with more elements', () => {
+      beforeEach(() => {
+        list.push('hello');
+        list.push('darkness');
+        list.push('my');
+        list.push('old');
+        list.push('friend');
+      });
+
+      it.skip('changes the list length', () => {
+        expect(list.head.next.data).to.eq('darkness');
+        expect(list.length).to.eq(5);
+
+        list.delete('friend');
+        expect(list.length).to.eq(4);
+        list.delete('my');
+        expect(list.length).to.eq(3);
+        list.delete('happy');
+        expect(list.length).to.eq(3);
+      });
+
+      it.skip('resets the next property on the node before the deleted node', () => {
+        expect(list.head.next.data).to.eq('darkness');
+
+        list.delete('darkness');
+
+        expect(list.head.next.data).to.eq('my');
+
+        list.delete('my')
+
+        expect(list.head.next.data).to.eq('old')
+      });
+
+      it.skip('resets the list.head if deleting the first node', () => {
+        expect(list.head.data).to.eq('hello');
+        list.delete('hello');
+        expect(list.head.data).to.eq('darkness');
+      });
+
+    })
   });
 
-  it ('after prepend length should be 1', () => {
-    list.prepend(25);
+  describe('TO ARRAY', () => {
+    it.skip('converts to an array', () => {
+      expect(list.toArray()).to.deep.equal([]);
+    });
 
-    expect(list.length).to.equal(1);
+    context('when there are several elements', () => {
+      beforeEach(() => {
+        list.push('The');
+        list.push('rain');
+        list.push('in');
+        list.push('Spain');
+      });
+
+      it.skip('can convert to an array', () => {
+        expect(list.toArray()).to.deep.equal(['The', 'rain', 'in', 'Spain']);
+      });
+    });
   });
 
-  it ('after append length should be 1', () => {
-    list.append(25);
+  describe('INCLUDES', () => {
+    beforeEach(() => {
+      list.push('The');
+      list.push('rain');
+      list.push('in');
+      list.push('Spain');
+    });
 
-    expect(list.length).to.equal(1);
+    it.skip('should return true if node is in list', () => {
+      expect(list.include("rain")).to.eq(true);
+    });
+
+    it.skip('should return false if node is not in list', () => {
+      expect(list.include("nope")).to.eq(false);
+    });
   });
 
-  it ('after append, then shift length should be 0', () => {
-    list.prepend(25);
-    list.shift();
+  describe('INDEX', () => {
+    beforeEach(() => {
+      list.push('oh');
+      list.push('hello');
+      list.push('world');
+    });
 
-    expect(list.length).to.equal(0);
+    it.skip('should return expected indexes', () => {
+      expect(list.index('oh')).to.eq(0);
+      expect(list.index('world')).to.eq(2);
+      expect(list.index('nope')).to.eq(null);
+    });
   });
 
-  it ('after append, then pop length should be 0', () => {
-    list.append(25);
-    list.pop();
+  describe('INSERT', () => {
+    beforeEach(() => {
+      list.push('dark');
+      list.push('stormy');
+    });
 
-    expect(list.length).to.equal(0);
+    it.skip('should insert nodes', () => {
+      expect(list.length).to.eq(2);
+      list.insert(1, 'and');
+      list.insert(3, 'night');
+      expect(list.length).to.eq(4);
+      expect(list.toArray()).to.deep.equal(['dark', 'and', 'stormy', 'night']);
+    });
   });
 
-  it ('toArray should return an array with all the items', () => {
-    list.append(25);
-    list.append(26);
-    list.append(253);
-    list.append(25123);
+  describe('INSERT AFTER', () => {
+    beforeEach(() => {
+      list.push('dark');
+      list.push('stormy');
+    });
 
-    let array = list.toArray();
-
-    expect(array).to.deep.equal([
-      list.find(25),
-      list.find(26),
-      list.find(253),
-      list.find(25123)
-    ]);
+    it.skip('should insert nodes after other nodes', () => {
+      expect(list.length).to.eq(2);
+      list.insertAgfter('dark', 'and');
+      list.insertAfter('stormy', 'night');
+      expect(list.length).to.eq(4);
+      expect(list.toArray()).to.deep.equal(['dark', 'and', 'stormy', 'night']);
+    });
   });
 
-  it ('toArray should return an array with all the items', () => {
-    let autoPopList = new LinkedList([
-      223,
-      2,
-      23,
-      2232
-    ]);
+  describe('DISTANCE', () => {
+    beforeEach(() => {
+      list.push("hello")
+      list.push("pizza")
+      list.push("world")
+      list.push("today")
+      list.push("tomorrow")
+    });
 
-    expect(autoPopList.length).to.equal(4)
+    it.skip('should calculate distance between nodes', () => {
+      expect(list.distance("hello", "today")).to.eq(3);
+      expect(list.distance("pizza", "today")).to.eq(2);
+      expect(list.distance("hello", "world")).to.eq(2);
+      expect(list.distance("hello", "tomorrow")).to.eq(4);
+      expect(list.distance("world", "today")).to.eq(1);
+    });
   });
-
-})
+});
---
+++ tests/Node-test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import Node from '../scripts/Node'
+
+describe('NODE', () => {
+  let node;
+
+  beforeEach(() => {
+    node = new Node('pizza')
+  })
+
+  it('should be a thing', () => {
+    expect(node).to.exist
+  })
+
+  it('should default next to null', () => {
+    expect(node.next).to.equal(null);
+  })
+
+  it('should take data and assign it to data prop', () => {
+    expect(node.data).to.equal('pizza')
+  })
+
+})
